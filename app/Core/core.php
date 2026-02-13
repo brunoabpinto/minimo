377231935f7d9cb3c4f17fdc7ebd99215a9e9776
Bruno Pinto <bruno.pinto@inboxagency.com>
2026-02-13 19:13:20 +0000

    changes

--- app/Core/core.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-(new Minimo\Core\Kernel())->run();
--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../app/Core/core.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+(new Minimo\Core\Kernel())->run();
